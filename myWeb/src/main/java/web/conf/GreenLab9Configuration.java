c4fb227881b3906f43422cccd820c42e6144cb24
khkim <hobbitbears@gmail.com>
2020-08-07 16:38:00 +0900

    [김경화] DB 접근주소 및 설정 추가
    - DB 접근주소 및 설정 추가

---
+++ myWeb/src/main/java/web/conf/GreenLab9Configuration.java
@@ -0,0 +1,10 @@
+package web.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+// @EnableSpringDataWebSupport
+public class GreenLab9Configuration extends WebMvcConfigurationSupport {
+
+}
